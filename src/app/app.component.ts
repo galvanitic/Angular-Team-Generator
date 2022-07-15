ce0b3f97dd01556ba9c5bfe6ad1a59b659c31c58
Rodolfo Galvan Martinez <rodolfom1@rodolfom1-a01.vmware.com>
2022-07-14 19:05:36 -0500

    Completed team generation logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,49 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = "";
   currMembers: string[] = [];
+  errorMessage = "";
+  numberOfTeams: number | "" = "";
+  teams: string[][] = [];
   addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty.";
+      return;
+    }
+    this.errorMessage = "";
     this.currMembers.push(this.newMemberName);
     this.newMemberName = "";
   }
   onInput(member:string) {
     this.newMemberName = member;
+  }
+  onNumberOfTeamsInput(value:string) {
+    this.numberOfTeams = Number(value);
+  }
+  generateTeams() {
+    console.log('hit')
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = "Invalid number of teams.";
+      return;
+    }
+    if(this.currMembers.length < this.numberOfTeams) {
+      this.errorMessage = "Not enough members.";
+      return;
+    }
+    this.errorMessage = '';
+    const allMembers = [...this.currMembers];
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const rand = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(rand, 1)[0];
+        if (!member) break;
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+    this.currMembers = [];
+    this.numberOfTeams = "";
   }
 }
